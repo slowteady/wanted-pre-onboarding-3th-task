0f04c582b632524e8a4fe496d4061e9f58d396f6
slowteady <vbn0213@gmail.com>
2023-09-05 22:56:33 +0900

    refactor: 스타일 관련 컴포넌트 불필요해져 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
-import { styled } from 'styled-components';
 import Router from './router/Router';
 
 function App() {
   return (
     <BrowserRouter>
-      <ToCenterWrap>
-        <Router />
-      </ToCenterWrap>
+      <Router />
     </BrowserRouter>
   );
 }
-
-const ToCenterWrap = styled.main`
-  display: flex;
-  height: 100vh;
-  align-items: center;
-  justify-content: center;
-`;
 
 export default App;
