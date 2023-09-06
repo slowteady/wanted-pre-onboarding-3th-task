feb291daaa669d89333a97ae7cdbac97772b86e6
slowteady <vbn0213@gmail.com>
2023-09-06 16:31:17 +0900

    feat: 노데이터 컴포넌트 추가

---
+++ src/components/utils/NoData.tsx
@@ -0,0 +1,20 @@
+import { styled } from 'styled-components';
+
+interface textProps {
+  text?: string;
+}
+
+const DEAULT_TEXT = '검색어가 없습니다.';
+
+function NoData({ text = DEAULT_TEXT }: textProps) {
+  return <NoDataDiv>{text}</NoDataDiv>;
+}
+
+const NoDataDiv = styled.div`
+  align-items: center;
+  line-height: 20;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export default NoData;
